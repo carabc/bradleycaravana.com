a1762c1955574a0d5797661b9696e8675ae8a9cf
Bradley Caravana <bcaravana@maximgrp.com>
2024-04-06 21:01:54 -0400

    Migrated database solution from planetscale to mongodb due to planetscale hobby tier being discontinued

--- pages/api/views/index.js
+++ pages/api/views/index.js
@@ -1,10 +1,14 @@
 import { PrismaClient } from "@prisma/client";
+import dbConnect from "../../../lib/mongo";
+import View from "../../../models/View";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   try {
-    const allViews = await prisma.tblViews.findMany();
+    await dbConnect();
+    // const allViews = await prisma.tblViews.findMany();
+    const allViews = await View.find({});
 
     return res.status(200).json(allViews);
   } catch (error) {
